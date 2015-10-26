4262d559f6e1ec447d765dd53eb94aa39e3526b6
Parameswara Rao Mallela <parameswararao.mallela@my.liu.edu>
2015-10-26 18:20:58 -0400

    Created Project
    Created Student Registration Project

---
+++ StudentRegistrationPortal/StudentRegistrationPortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentRegistrationPortal.Startup))]
+namespace StudentRegistrationPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
