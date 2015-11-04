d0017b1620a75e13c897812179788769e7de2760
Parameswara Rao Mallela <parameswararao.mallela@my.liu.edu>
2015-11-04 18:05:06 -0500

    Created Project

--- StudentRegistrationPortal/StudentRegistrationPortal/Properties/AssemblyInfo.cs
+++ StudentRegistrationPortal/StudentRegistrationPortal/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1e942f85-7ace-4633-8638-d64ef02d5b5f")]
+[assembly: Guid("8708bc53-bd46-4595-8384-fc8eff02c56e")]
 
 // Version information for an assembly consists of the following four values:
 //
